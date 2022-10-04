ddd0a01b0a9b53de459f9436aae0da7f7820fb1c
HECTOR JOSUE GONZALEZ CORTES <62352013+iHector1@users.noreply.github.com>
2022-10-03 21:09:04 -0500

    update page, add taskcli

--- pages/works.js
+++ pages/works.js
@@ -1,22 +1,28 @@
-import {Heading, SimpleGrid,Container } from '@chakra-ui/react'
+import { Heading, SimpleGrid, Container } from '@chakra-ui/react'
 import Section from '../components/section'
 import { WorkGridItem } from '../components/grid-item'
 import thumbYummy from '../public/images/img_yummy.png'
 import thumbRick from '../public/images/img_rick.png'
+import thumbTask from '../public/images/img_task.png'
 const Works = () => {
     return (
         <Container >
             <Heading as="h3" fontSize={20} mb={6} variant="section-title">Works
             </Heading>
             <SimpleGrid columns={[1, 1, 2]} gap={6}>
-                <Section> 
+                <Section>
                     <WorkGridItem id="yummy" title="Yummy" thumbnail={thumbYummy}>
-                        A social media about kitchen recipies. 
+                        A social media about kitchen recipies.
                     </WorkGridItem>
                 </Section>
-                <Section> 
+                <Section>
                     <WorkGridItem id="rick" title="Ricardo y Martin APP" thumbnail={thumbRick}>
                         Rick and Morty&apos;s api client, it show all characters of the serie and it&apos;s characteristics and it&apos;s PWA application
+                    </WorkGridItem>
+                </Section>
+                <Section>
+                    <WorkGridItem id="taskcli" title="TaskCli" thumbnail={thumbTask}>
+                        TaskCli is a terminal program that&apos;s solve user&apos;s administration task.
                     </WorkGridItem>
                 </Section>
             </SimpleGrid>
---
+++ pages/works/taskcli.js
@@ -0,0 +1,37 @@
+import { Badge, List, ListItem, Box } from "@chakra-ui/react";
+import { Title, Meta } from '../../components/works'
+import P from '../../components/paragraph'
+import Layout from '../../components/layouts/article'
+
+const Work = () => {
+    return (
+        <Layout title="Rick">
+            <Title>
+                TaskCli<Badge>2022</Badge>
+            </Title>
+            <P>
+                It&apos;s a terminal program that provide user&apos;s administration task, the user will administrate his activities, he can create activities, delete activities, update activities and edit activities, this application is conected to a database in mongoDB.
+            </P>
+            <List ml={4} my={4}>
+                <ListItem>
+                    <Meta>Platform</Meta>
+                    <span>Windows/macOS/Linux</span>
+                </ListItem>
+                <ListItem>
+                    <Meta>Stack</Meta>
+                    <span>NodeJS, mongoDB, terminal.</span>
+                </ListItem>
+            </List>
+            <Box
+                as='iframe'
+                src='https://www.youtube.com/embed/A3_Ucg9y1UI'
+                width='100%'
+                sx={{
+                    aspectRatio: '16/9'
+                }}
+            />
+        </Layout>
+    )
+}
+
+export default Work
